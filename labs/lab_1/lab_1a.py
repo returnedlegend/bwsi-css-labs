f6e06eac870ac13f12453d3ce91edfa4eff2fc41
returnedlegend <ramanjamal204@gmail.com>
2026-03-08 18:18:26 -0600

    Added discussion post as a print statement

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Raman Jamal" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone, my name is Raman Jamal. I am currently a sophomore in Bryan Texas. I was born in Iraq but moved to the U.S. recently. In my free time, I enjoy learning math, physics, and coding for which I spend time preparing for competitions. I also do some web development and play soccer when I am bored.")
 
 if __name__ == "__main__":
     main()
